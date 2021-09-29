a14de3f8a1da48fc312c3d0b1e562bec92206652
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-30 00:13:06 +0600

    Project completed..

--- ThreadPerformingDifferentTasks/Program.cs
+++ ThreadPerformingDifferentTasks/Program.cs
@@ -21,10 +21,11 @@ namespace ThreadPerformingDifferentTasks
     {
         static void Main(string[] args)
         {
+            //create threads
             Thread t1 = new Thread(new ThreadStart(MyThread.Thread1));
             Thread t2 = new Thread(new ThreadStart(MyThread.Thread2));
 
-
+            //start all threads
             t1.Start();
             t2.Start();
 
