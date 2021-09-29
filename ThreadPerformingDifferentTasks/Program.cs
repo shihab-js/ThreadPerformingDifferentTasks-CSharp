aba255c62c01fc3e38447f20703deacad2a3e0dd
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-30 00:04:03 +0600

    New class MyThread has been created..

--- ThreadPerformingDifferentTasks/Program.cs
+++ ThreadPerformingDifferentTasks/Program.cs
@@ -1,4 +1,19 @@
 ﻿using System;
+using System.Threading;
+
+
+public class MyThread
+{
+    public static void Thread1()
+    {
+        Console.WriteLine("This is task one!");
+    }
+
+    public static void Thread2()
+    {
+        Console.WriteLine("This is task two!");
+    }
+}
 
 namespace ThreadPerformingDifferentTasks
 {
