497109e6e24db7dd894d32ab845de56eb785042f
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-30 00:11:14 +0600

    Threads are started...

--- ThreadPerformingDifferentTasks/Program.cs
+++ ThreadPerformingDifferentTasks/Program.cs
@@ -24,8 +24,10 @@ namespace ThreadPerformingDifferentTasks
             Thread t1 = new Thread(new ThreadStart(MyThread.Thread1));
             Thread t2 = new Thread(new ThreadStart(MyThread.Thread2));
 
-          
-                
+
+            t1.Start();
+            t2.Start();
+
         }
     }
 }
